e23666559ab04b4ff96e1879356dfc4faaecd418
bovvsr45 <bovvsr45@gmail.com>
2024-06-23 19:11:13 +0900

    making section

--- src/App.js
+++ src/App.js
@@ -10,19 +10,27 @@ import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
 
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
+
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/today' element={<Today/>} />
-                <Route path='/developer' element={<Dev/>} />
-                <Route path='/youtube' element={<Yt/>} />
-                <Route path='/channel/:handle' element={<Channel/>} />
-                <Route path='/video/:videoID' element={<Video/>} />
-                <Route path='/search/:searchID' element={<Search/>} />
-                <Route path='/*' element={<Not/>} />
-            </Routes>
+            <Header />
+            <Main>
+                <Routes>
+                    <Route path='/' element={<Home/>} />
+                    <Route path='/today' element={<Today/>} />
+                    <Route path='/developer' element={<Dev/>} />
+                    <Route path='/youtube' element={<Yt/>} />
+                    <Route path='/channel/:handle' element={<Channel/>} />
+                    <Route path='/video/:videoID' element={<Video/>} />
+                    <Route path='/search/:searchID' element={<Search/>} />
+                    <Route path='/*' element={<Not/>} />
+                </Routes>
+            </Main>
+            <Footer />
         </BrowserRouter>
     )
 }
