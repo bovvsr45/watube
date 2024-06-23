d87de0fe35feada2cb20c700489d0f5443e8cc3e
bovvsr45 <bovvsr45@gmail.com>
2024-06-23 18:49:03 +0900

    pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Dev from './pages/Dev'
+import Yt from './pages/Yt'
+import Channel from './pages/Channel'
+import Video from './pages/Video'
+import Search from './pages/Search'
+import Not from './pages/Not'
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/today' element={<Today/>} />
+                <Route path='/developer' element={<Dev/>} />
+                <Route path='/youtube' element={<Yt/>} />
+                <Route path='/channel/:handle' element={<Channel/>} />
+                <Route path='/video/:videoID' element={<Video/>} />
+                <Route path='/search/:searchID' element={<Search/>} />
+                <Route path='/*' element={<Not/>} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/pages/Yt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Yt = () => {
+    return (
+        <div>Yt</div>
+    )
+}
+
+export default Yt
